80062470fcdc0ddbcb036ebbf5adcc7af3bb6a2d
Pranav Atote <123180829+PranavAI2050@users.noreply.github.com>
2025-07-24 15:37:47 +0545

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const upload = multer({ dest: "uploads/" }); // Temp folder for uploaded images
 app.use(cors());
 app.use(express.json({ limit: "25mb" }));
 
-const PORT = 5000;
+// const PORT = 5000;
+const PORT = process.env.PORT || 5000 
+
 const GEMINI_KEY = process.env.GEMINI_KEY;
 
 // Validate API key at startup
@@ -33,26 +35,6 @@ app.post("/extract-info", upload.single("image"), async (req, res) => {
     }
 
     const genAI = new GoogleGenerativeAI(GEMINI_KEY);
-
-//     const PROMPT = `
-// You are a document parser. Analyze this driving license image and extract the following fields only:
-
-// Return the result as **valid JSON** with these exact keys:
-
-// {
-//   "full_name": "...",
-//   "license_number": "...",
-//   "date_of_birth": "...",
-//   "nationality": "...",
-//   "license_expiry_date": "...",
-//   "license_issue_date": "...",
-//   "place_of_issue": "..."
-// }
-
-// ⚠️ If any value is missing or unreadable due to blur, occlusion, or noise, return: "failed to get" as the value for that field.
-
-// Don't include any explanation. Only return the JSON object.
-// `;
 
 const PROMPT = `
 You are a document parser. Analyze this driving license image and extract the following fields only.
